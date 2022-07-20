246bb0be14b4954d781b758c0d395a25d4b8e436
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-20 20:02:02 -0300

    ci(debug): debugging the configuration
    debugging the configuration

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({ message: "Hello, world!" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+  return res.json({ name });
 });
 
 app.listen(3333);
