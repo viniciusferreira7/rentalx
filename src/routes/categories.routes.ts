3cb4c5cb299e39fc100bdc13cc3caaf4b51313a9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-28 21:09:12 -0300

    feat(categoriesrepository.ts): list of methods created inside CategoryRepository.ts
    list of methods created inside CategoryRepository.ts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post("/", (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get("/", (request, response) => {
+  const all = CategoryRepository.list();
+
+  return response.json(all);
+});
+
 export { categoriesRoutes };
